bab543bb8505d418fa8e3165a2289807cc092c5c
Rohan Narayan <rrnarayan1@gmail.com>
2026-08-22 18:04:29 -0700

    (7) (1.1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.rohanNarayan.omnicrosswords"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
